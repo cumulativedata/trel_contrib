d1adde9827d25b1d6f96228104e2d12084e24ebd
Gautham Anil <gautham.anil@gmail.com>
2022-05-10 00:23:45 +0000

    ODBC sensor inherits from ClockBasedSensor. supports load_info updates.

--- sensors/odbc_table_load/odbc_table_load.py
+++ sensors/odbc_table_load/odbc_table_load.py
@@ -23,6 +23,13 @@ The behavior is as follows:
  * cron_constraint
  * delay_seconds
  * max_instance_age_seconds
+ * backfill_newest_first (true / false)
+
+ Here, treat the ``cron_constraint`` as a set rather than a timer. It consists of a set of timestamps. The ``cron_constraint`` defines the periods. Each period starts at a timestamp in the ``cron_constraint`` and ends before the next one. 
+
+This sensor will try to create a dataset for each period. The resulting dataset's ``instance_ts`` will match the period start.
+ 
+ A period is considered ready to load ``delay_seconds`` after the end of the period. Sensor will not try to load a period whose start is older than ``max_instance_age_seconds`` from now.
 
 3. Once a dataset that should be created is identified, a connection is established to the ODBC data source
 
@@ -49,6 +56,18 @@ The behavior is as follows:
 
     This restriction does not apply to object store destinations such as S3 and Google Storage.
 
+Custom SQL
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+``custom_sql`` that is provided will be formatted using python's ``.format`` rather than ODBC's SQL parameterization. This is largely for convenience and readability. The rationale for not worrying about SQL injestion is that the SQL is provided by the developer and parameterized by Trel using strings not selectable by anyone. 
+
+Only three parameters are possible:
+
+1. ``table``: This is the table selected by the developer in this config.
+2. ``instance_ts``: This corresponds to the dataset and is a datetime object
+3. ``instance_ts_precision``: This takes one of 4 values: 'D', 'H', 'M' and 'S'.
+4. ``period_end``: This is the end of this period. The period starts at ``instance_ts``.
+
 Please take a look at :ref:`odbc_data_sources` for a list of pre-installed ODBC drivers in the Trel instance for the ``driver`` parameter.
 
 Supported repository classes as destination:
@@ -56,19 +75,15 @@ Supported repository classes as destination:
 1. ``s3``
 2. ``bigquery``
 
-
 '''
-
 
 import argparse, os, sys
 import treldev, pyodbc, tempfile, json, datetime, subprocess
 from os import listdir
 from os.path import isfile, join, isdir
 
-class ODBCSensor(treldev.Sensor):
+class ODBCSensor(treldev.ClockBasedSensor):
 
-
-    
     def __init__(self, config, credentials, *args, **kwargs):
         super().__init__(config, credentials, *args, **kwargs)
         
@@ -97,15 +112,9 @@ class ODBCSensor(treldev.Sensor):
         self.lookback_seconds = self.config['max_instance_age_seconds'] - 1 # how far we should backfill missing datasets
         self.locking_seconds = self.config.get('locking_seconds',600)
     
-    def get_new_datasetspecs(self, datasets, **kwargs):
-        # res = list( self.get_new_datasetspecs_with_cron_and_precision(datasets) )
-        # self.logger.debug(res)
-        # return res
-        return self.get_new_datasetspecs_with_cron_and_precision(datasets)
-    
     def save_data_to_path(self, load_info, uri, dataset=None, **kwargs):
         ''' if the previous call to get_new_datasetspecs returned a (load_info, datasetspec) tuple, then this call should save the data to the provided path, given the corresponding (load_info, path). '''
-        minute = load_info
+        minute = load_info['instance_ts']
 
         cnxn = pyodbc.connect('DRIVER={'+self.driver+'};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
         cursor = cnxn.cursor()
@@ -120,12 +129,13 @@ class ODBCSensor(treldev.Sensor):
         destination.prepare()
             
         fetch_rows = self.batch_rows // 20
-        sql = (f"select * from {self.table}"
+        sql = ("select * from {table}"
                if self.custom_sql is None
                else self.custom_sql)
         args = {'table': self.table,
                 'instance_ts':minute,
-                'instance_ts_precision':self.instance_ts_precision}
+                'instance_ts_precision':self.instance_ts_precision,
+                'period_end': load_info['period_end']}
         print("SQL", sql.format(**args), file=sys.stderr)
         cursor.execute(sql.format(**args))
 
