78c63011ffeb1b6fcebb3a6b02aa5540a5b2919a
Gautham Anil <gautham.anil@gmail.com>
2023-04-10 16:46:53 +0000

    Append day update to include schema check

--- job_examples/bigquery/append_day/append_day.py
+++ job_examples/bigquery/append_day/append_day.py
@@ -23,4 +23,6 @@ CROSS JOIN max_
 WHERE ts >= DATETIME_SUB(max_ts, INTERVAL 29 DAY)
 """)
 
-    output_bq.validate(args, save=True, sodacore_check=True)
+    output_bq.validate(args, save=True,
+                       sodacore_check=True,
+                       sodacore_schema_check=True)
