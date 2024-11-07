01265f018fce7c991f493ef4a3fd3394a57ab7d4
Saawan Fultariya <saawan.fp77@gmail.com>
2024-11-07 13:16:26 +0000

    athena improvements

--- lifecycle/athena/athena.py
+++ lifecycle/athena/athena.py
@@ -9,7 +9,7 @@ import datetime
 from multiprocessing import Pool, cpu_count
 from typing import Dict, Any, List, Tuple
 from treldev import get_args, S3Commands
-from athena_utils import AthenaURI  # Assuming your Athena utility classes are in athena_utils.py
+from treldev.awsutils import AthenaURI  # Assuming your Athena utility classes are in athena_utils.py
 import logging
 
 # Configure logging
